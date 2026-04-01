9afc34f7496672cf24019408f2e3df4ec6f55b75
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2026-04-01 09:39:49 +0300

    chore: add eslint rule for no-magic-numbers and use constants

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,10 +26,21 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+      'no-magic-numbers': [
+        'warn',
+        {
+          ignore: [0, 1],
+          ignoreArrayIndexes: true,
+          enforceConst: true,
+          detectObjects: false,
+        },
+      ],
+
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
